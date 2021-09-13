e9e1465ebe1c541403b282ed5f5bbaf1d7d9bb8e
behnam <bihnam998@gmail.com>
2021-09-13 15:51:26 +0430

    add

--- code_t.go
+++ code_t.go
@@ -2,26 +2,20 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"time"
 )
 
-type Student struct {
-	score1, score2, score3 float64
-}
+var my int=0
 
-func (s Student) Average() float64 {
-	return (s.score1 + s.score2 + s.score3) / 3
-}
-
-func (s Student) Min() float64 {
-	return math.Min(s.score1, math.Min(s.score2, s.score3))
-}
-
-func (s Student) Max() float64 {
-	return math.Max(s.score1, math.Max(s.score2, s.score3))
+func say() {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(my)
+		my++
+	}
 }
 
 func main() {
-	s1 := Student{12.25, 15.75, 20}
-	fmt.Println("Min :", s1.Min(), "| Max :", s1.Max(), "| Ave. :", s1.Average())
+	go say()
+	say()
 }
